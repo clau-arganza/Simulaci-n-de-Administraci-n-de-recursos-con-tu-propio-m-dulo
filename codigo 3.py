a06da7a9fd727bcdf5845fd2983e120c3fc5cfab
Franalma88 <falmavaz@myuax.com>
2025-09-21 19:23:19 +0200

    re.codigo

---
+++ codigo 3.py
@@ -0,0 +1,39 @@
+MATERIALES = ["ladrillo", "ventanas", "rejas", "cemento", "madera"]
+
+recursos = [800, 100, 120, 500, 500]
+edificios = {
+        "Zara":             [200, 10, 0,   100, 50],
+        "tienda de cómics": [100,  2, 1,    50, 25],
+        "manicomio":        [300, 30, 100, 150, 100],
+    }
+
+
+
+def mostrar_recursos():
+    print("\n=== Recursos disponibles ===")
+    for nombre, cant in zip(MATERIALES, recursos):
+        print(f"- {nombre}: {cant}")
+    
+ 
+def mostrar_edificios():
+    print("\n=== Edificios disponibles (costes) ===")
+    for i, (nombre, coste) in enumerate(edificios.items(), start=1):
+        coste_str = ", ".join(f"{m}:{c}" for m, c in zip(MATERIALES, coste))
+        print(f"{i}. {nombre} -> {coste_str}")
+
+def buscar_edificios(nombre): 
+    for edificio in edificios:
+        if nombre==edificio.lower():
+            print(nombre)
+        
+def main():
+    mostrar_recursos()
+    mostrar_edificios()
+    
+    eleccion = input("\nElige un edificio (número o nombre): ").strip().lower()
+    
+    buscar_edificios(eleccion)
+    
+    
+if __name__ == "__main__":
+    main()
