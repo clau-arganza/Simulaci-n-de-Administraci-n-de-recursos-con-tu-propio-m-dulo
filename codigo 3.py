ccea091ef7eb5251be3e1f50f58b70d8eb168490
Franalma88 <falmavaz@myuax.com>
2025-09-23 14:52:34 +0200

    refresh

--- codigo 3.py
+++ codigo 3.py
@@ -2,11 +2,11 @@ MATERIALES = ["ladrillo", "ventanas", "rejas", "cemento", "madera"]
 
 recursos = [800, 100, 120, 500, 500]
 edificios = {
-        "Zara":             [200, 10, 0,   100, 50],
-        "tienda de cómics": [100,  2, 1,    50, 25],
+        "zara":             [200, 10, 1,   100, 50],
+        "tienda de cómics": [100,  2, 5,    50, 25],
         "manicomio":        [300, 30, 100, 150, 100],
     }
-
+edificios_creados=[]
 
 
 def mostrar_recursos():
@@ -17,34 +17,56 @@ def mostrar_recursos():
  
 def mostrar_edificios():
     print("\n=== Edificios disponibles (costes) ===")
-    for i, (nombre, coste) in enumerate(edificios.items(), start=1):
-        coste_str = ", ".join(f"{m}:{c}" for m, c in zip(MATERIALES, coste))
-        print(f"{i}. {nombre} -> {coste_str}")
+    for (nombre, coste) in edificios.items():
+        lista_materiales=""
+        for m, c in zip(MATERIALES, coste):
+            lista_materiales+=", "+f"{m}:{c}"
+        print(f"{nombre} -> {lista_materiales}")
+        
 
 def buscar_edificios(nombre): 
-    if nombre.isdigit():
-        
-        if int(nombre)>0 and int(nombre)<len(edificios)+1:
-            
-            print(f"  {edificios['tienda de cómics']}")
-        else:
-            print(" no correcto")    
-    else: 
-        for edificio in edificios:
-            if nombre==edificio.lower():
-                print(nombre)
-
-
     
-
-        
+    if nombre in edificios: 
+        cant_mat=edificios[nombre]
+        crear_edificio(cant_mat,nombre)    
+    else:          
+        print("no existe el Edificio")
+    
+def crear_edificio(cant_mat,nombre):
+    suficiente=1
+    for e,r in zip(cant_mat,recursos):
+        if not r>=e:
+            print ("No tienes Materiales suficientes")
+            suficiente=0
+    if suficiente>0:
+        for x, valor in enumerate(recursos):
+            valor-=cant_mat[x]
+            if valor>-1 :
+                recursos[x]=valor
+        edificios_creados.append(nombre) 
+        if  len(edificios_creados) > 1:
+            print(f"\n=== Edificios creados es {edificios_creados}  ===\n")    
+        else:    
+            print(f"\n=== El edificio creado es {edificios_creados}  ===\n")        
+        print("\n=== Materiales restantes ===")
+        for nombre, cant in zip(MATERIALES, recursos):
+            print(f"- {nombre}: {cant}")   
+    
+    
+    
 def main():
-    mostrar_recursos()
-    mostrar_edificios()
+    while True:
+        mostrar_recursos()
+        mostrar_edificios()
     
-    eleccion = input("\nElige un edificio (número o nombre): ").strip().lower()
+        eleccion = input("\nElige un edificio (nombre): ").lower()
     
-    buscar_edificios(eleccion)
+        buscar_edificios(eleccion)
+        sino=input("quieres seguir construyendo edificios S/N").lower()
+       
+        if sino == "n":
+            break
+        
     
     
 if __name__ == "__main__":
